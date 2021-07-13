1a28b5068ff33305055ae3b877e4721527534492
kimck <kimck@ecosian.com>
2021-07-13 23:45:26 +0900

    first commit

---
+++ src/main/java/com/example/demo/ShopVueFront1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopVueFront1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopVueFront1Application.class, args);
+	}
+
+}
